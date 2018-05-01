08d302991a28d22129b09d501eb7f48c2eb5ae58
Zheng-Ling Lai <jamesljlster@gmail.com>
2018-05-01 18:41:24 +0800

    Finish cmake member check

--- cmake/CheckMacro.cmake
+++ cmake/CheckMacro.cmake
@@ -4,3 +4,10 @@ macro(CHECK_TYPE TYPE INCLUDE_PATH VAR)
 		int main(void) { ${TYPE} tmp; return 0; }"
 		${VAR})
 endmacro()
+
+macro(CHECK_MEMBER TYPE MEMBER INCLUDE_PATH VAR)
+	CHECK_C_SOURCE_COMPILES(
+		"#include ${INCLUDE_PATH}
+		int main(void) { ((${TYPE}*)0)->${MEMBER}; return 0; }"
+		${VAR})
+endmacro()
